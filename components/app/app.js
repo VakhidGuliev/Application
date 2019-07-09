bb2fb60e4d713794df65aaa28251f8a43fa971dd
VugarWeb777 <vugarguliev333@gmail.com>
2019-07-09 16:38:14 +0300

    Created method getTask

--- components/app/app.js
+++ components/app/app.js
@@ -1,34 +1,41 @@
 "use strict";
 
 var form = document.forms.namedItem("task");
+var tabNav = document.querySelectorAll("#myList a");
+
 
 var TaskForm = {
 
     date: function () {
         var formData = {
             name: form.elements.namedItem("name").value,
-            description: form.elements.namedItem("description").value,
-            categories: form.elements.namedItem("categories").querySelectorAll("option")
+            categoryName: TaskForm.getCategoryName(),
+            id: Math.round(Math.random() * (100 - 1) + 1),
+            Date: new Date().toLocaleDateString(),
+            Time: new Date().toLocaleTimeString()
         };
 
         return formData;
     },
 
+    getCategoryName: function () {
+        var tabName = document.querySelectorAll(".tab-content .tab-pane");
+        var categoriesName;
+
+        for (let i = 0; i < tabName.length; i++) {
+            if (tabName[i].classList.contains("active")) {
+                categoriesName = tabName[i].id;
+            }
+        }
+        return categoriesName;
+    },
 
     createTask: async function (event) {
         event.preventDefault();
 
-        var date = TaskForm.date();
-        var categoriesName;
+        let date = TaskForm.date();
 
-
-        for (let i = 0; i < date.categories.length; i++) {
-            if (date.categories[i].selected){
-                categoriesName= date.categories[i].value;
-            }
-        }
-
-        await add(date, `/Tasks/${categoriesName}.json`);
+        await add(date, `/Tasks/${date.categoryName}.json`);
 
         form.reset();
 
@@ -44,26 +51,32 @@ var TaskForm = {
         }, 2000)
 
     },
+
     getTasks: async function () {
+        let date = TaskForm.date();
 
+        let dataTask = await get(`/Tasks/${date.categoryName}.json`);
+        let promise = fbTransformToArray(dataTask);
 
-        var dataTask = await get(`/Tasks/${categoriesName}.json`);
-
-        var promise = fbTransformToArray(dataTask);
-
-        var taskList = document.querySelector(".task-list");
+        var tabContent = document.querySelector(".tab-content .tab-pane.active");
 
         promise.then(function (arr) {
 
             arr.forEach(function (item) {
-
-                var task = document.createElement("li");
-                task.innerText = item.name;
-                taskList.insertAdjacentElement("afterbegin", task);
+                var taskName = document.createElement("p");
+                taskName.innerText = item.name;
+                tabContent.insertAdjacentElement("afterbegin",taskName)
             })
         }).catch(e => console.log(e.message));
-
     },
 };
 
-form.addEventListener("submit",TaskForm.createTask);
+form.addEventListener("submit", TaskForm.createTask);
+
+tabNav.forEach(nav => {
+    nav.addEventListener("mousedown", function () {
+        if (nav.classList.contains("active")) {
+            TaskForm.getTasks();
+        }
+    })
+});
